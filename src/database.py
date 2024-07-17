1cd7f00002b648d34587c5e438d2e266d41edb48
menzo <menzo.smit-adam@epitech.eu>
2024-07-17 14:02:41 +0200

    db

--- src/database.py
+++ src/database.py
@@ -1,12 +1,15 @@
-import os
-from supabase import create_client
+import psycopg2
+from psycopg2 import sql
+from datetime import datetime
 
-def database_connect():
-    url = os.environ.get("https://amddqcbkilnmquaudlzu.supabase.co")
-    key = os.environ.get("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFtZGRxY2JraWxubXF1YXVkbHp1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjEyMDY0MTMsImV4cCI6MjAzNjc4MjQxM30.V_OAckK_eyeHInbQVpweyAynjWvgK81yNmggDSJfTAM")
-
-    if not url or not key:
-        raise ValueError("Supabase URL and key must be provided in environment variables.")
-
-    supabase = create_client(url, key)
-    return supabase
+def insert_log_into_db(ip, status, timestamp):
+    try:
+        connection = psycopg2.connect(
+            dbname:"",
+            user:"",
+            password:"",
+            host:"",
+            port:""
+        )
+        cursor = connection.cursor()
+        
